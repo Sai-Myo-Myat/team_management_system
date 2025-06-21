e1da1ac28bd415b92a264130638a19e0fef4fc54
Sai Myo Myat <saimyomyat511@gmail.com>
2025-06-21 12:48:17 +0700

    feat: teams table demo

--- src/app/(console)/teams/page.tsx
+++ src/app/(console)/teams/page.tsx
@@ -1,10 +1,51 @@
+import { DataTable } from "@/components/data-table";
 import React from "react";
+import { columns } from "./teams-table-columns";
 
 export default function TeamsPage() {
   return (
     <div>
       <h1>Teams</h1>
-      <p>This is the teams page.</p>
+      <DataTable
+        columns={columns}
+        data={[
+          {
+            id: "1",
+            name: "Team One",
+            country: "Thailand",
+            region: "Aisa",
+            player_count: 5,
+          },
+          {
+            id: "2",
+            name: "Team Two",
+            country: "USA",
+            region: "North America",
+            player_count: 4,
+          },
+          {
+            id: "3",
+            name: "Team Three",
+            country: "Germany",
+            region: "Europe",
+            player_count: 6,
+          },
+          {
+            id: "4",
+            name: "Team Four",
+            country: "Brazil",
+            region: "South America",
+            player_count: 3,
+          },
+          {
+            id: "5",
+            name: "Team Five",
+            country: "Australia",
+            region: "Oceania",
+            player_count: 7,
+          },
+        ]}
+      />
     </div>
   );
 }
