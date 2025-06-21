ed59f2312098c0bd468dec5b3f266f4efb784fa3
Sai Myo Myat <saimyomyat511@gmail.com>
2025-06-21 13:50:59 +0700

    feat: actions column demo

---
+++ src/app/(console)/teams/teams-table-columns.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import ActionColumn from "@/components/table-actions-column";
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+
+export type Team = {
+  id: string;
+  name: string;
+  player_count: number;
+  region: string;
+  country: string;
+};
+
+export type TeamsTableMeta = {
+  updateTeam: (id: string) => void;
+  removeTeam: (id: string) => void;
+};
+
+export const columns: ColumnDef<Team>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "player_count",
+    header: "Player Count",
+  },
+  {
+    accessorKey: "region",
+    header: "Region",
+  },
+  {
+    accessorKey: "country",
+    header: "Country",
+  },
+  {
+    accessorKey: "actions",
+    header: () => <div></div>,
+    cell: ({ row, table }) => (
+      <ActionColumn
+        row={row}
+        renderMenuItems={(row) => (
+          <>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() =>
+                (table.options.meta as TeamsTableMeta).updateTeam(
+                  row.getValue("id")
+                )
+              }
+            >
+              Update
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="text-destructive font-semibold cursor-pointer"
+              onClick={() =>
+                (table.options.meta as TeamsTableMeta).removeTeam(
+                  row.getValue("id")
+                )
+              }
+            >
+              Remove
+            </DropdownMenuItem>
+          </>
+        )}
+      />
+    ),
+  },
+];
